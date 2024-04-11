bdcfe985842ee6641bb07d3dc07ee999e60e169e
katarinaneal <166740315+katarinaneal@users.noreply.github.com>
2024-04-11 12:53:47 -0400

    Update kat_Lab9.py

--- kat_Lab9.py
+++ kat_Lab9.py
@@ -1,3 +1,10 @@
+def encoder(message):
+    result = ''
+    for digit in message:
+        new_digit = str((int(digit) + 3) % 10)
+        result += new_digit
+    return result
+
 def decoder(password):
     result = ''
     for digit in password:

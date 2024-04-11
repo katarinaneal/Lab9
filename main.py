1280702f53ba1fa754dc084a2882defe816ee5b7
shirleymaza <166472849+shirleymaza@users.noreply.github.com>
2024-04-11 16:02:26 -0400

    shirley - added menu to main

--- main.py
+++ main.py
@@ -14,3 +14,12 @@ def decoder(password):
         result += new_digit
     return result
     
+
+#shirley - added the menu
+def menu():
+    print("Menu")
+    print("-------------")
+    print("1. Encode")
+    print("2. Decode")
+    print("3. Quit")
+    pass
